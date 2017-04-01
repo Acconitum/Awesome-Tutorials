5ce40b3fea588ff4548ce61b6a2f0292e4775a41
Acconitum <marc.mentha@gmail.com>
2017-04-01 09:44:04 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Awesome Tutorials
+Text Domain: awesome-tutorials
+Plugin URI:
+Description:  A WordPress Plugin for tutorialsites
+Version: 0.1
+Author: Marc Mentha
+Author URI: http://cubetech.ch
+License: A "Slug" license name e.g. GPL2
+*/
